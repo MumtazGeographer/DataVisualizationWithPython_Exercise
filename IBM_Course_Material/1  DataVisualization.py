706f1821f06d7195787fefccc3ba2eb8f4d03d10
Mumtaz Ali <mumtazgeographer@gmail.com>
2025-02-09 06:12:39 -0800

    Lab Lab

--- IBM_Course_Material/1  DataVisualization.py
+++ IBM_Course_Material/1  DataVisualization.py
@@ -3,6 +3,33 @@ import pandas as pd # primary data structure library
 
 excelsheet = 'Canada.xlsx'
 
-df_can = pd.read_excel(excelsheet, sheet_name="Candana by Citizenship",skiprows=range(20), skipfooter=2)
+#df_can = pd.read_excel(excelsheet, sheet_name="Candana by Citizenship",skiprows=range(20), skipfooter=2)
+df_can = pd.read_excel(excelsheet, sheet_name="Canada by Citizenship", skiprows=range(20), skipfooter=2)
+print(df_can.head())
 
-print(df.head())
+print(df_can.tail())
+
+print(df_can.info())
+
+print(df_can.columns)
+
+print(df_can.index)
+
+print(type(df_can.columns))
+print(type(df_can.index))
+
+print(df_can.columns.tolist())
+print(df_can.index.tolist())
+
+print(df_can.shape)
+
+df_can.drop(['AREA','REG','DEV','Type','Coverage'], axis=1, inplace=True)
+print(df_can.head(2))
+
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent', 'RegName':'Region'},inplace=True)
+print(df_can.columns)
+
+#df_can['Total'] = df_can.sum(axis=1)
+print(df_can.isnull().sum())
+
+print(df_can.describe())
